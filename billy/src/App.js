56643afe7a623bcf2eb636bb1f4292dbdf7553d6
Nishu4403 <nishathsulikeri@gmail.com>
2024-09-14 12:35:49 +0530

    Created App

---
+++ billy/src/App.js
@@ -0,0 +1,32 @@
+
+
+function App() {
+  return (
+    <div>
+    <div className="flex flex-nowrap flex-col items-center justify-center h-screen">
+      <div className="">
+        <p className="text-3xl font-bold underline text-center">
+          Billy
+        </p>
+      </div><br/>
+
+      <div className="">
+        <p className="text-center ">
+          Loading....
+        </p>
+      </div>
+
+      </div>
+
+      <div className="">
+      <p className="text-center ">
+        Loading....
+      </p>
+      </div>
+    
+    </div>
+    
+  );
+}
+
+export default App;
